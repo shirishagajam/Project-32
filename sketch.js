3b8bd03245a8a63ce56fee03438fb963d6696eac
YashGupta789 <74540040+YashGupta789@users.noreply.github.com>
2021-03-31 14:42:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -91,7 +91,7 @@ function detectcollision(dart,board){
 }
 
 async function getBackgroundImg(){
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
   var responseJSON = await response.json();
   var datetime = responseJSON.datetime;
   var hour = datetime.slice(11,13);
